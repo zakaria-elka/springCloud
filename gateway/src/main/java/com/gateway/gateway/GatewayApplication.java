afdc940cfa7cc4274af437becf723862d2727e59
zakaria-elka <zakaria.elkm@gmail.com>
2022-12-03 11:56:21 +0100

    gateway added

---
+++ gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
@@ -0,0 +1,32 @@
+package com.gateway.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	//@Bean
+	RouteLocator routeLocator(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+				.route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+				.route(r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
+				.build();
+	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient discoveryClient,
+															DiscoveryLocatorProperties properties)
+	{
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+	}
+}
